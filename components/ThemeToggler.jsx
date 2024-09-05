cc7f02fcc204558a19b8940568c5ab9e30ab9db1
Sergio Ayala <sergio.ayala@linktic.com>
2024-09-04 19:09:06 -0500

    first commit

---
+++ components/ThemeToggler.jsx
@@ -0,0 +1,13 @@
+'use client' ;
+
+import { Button from ' ./ui/button' ;
+import { Moonlcon, Sunlcon from '@radix-ui/react-icons' ;
+import { useTheme from 'next-themes' ;
+const ThemeToggIer =
+const { theme, set Theme
+return (
+<Sunlcon
+<MoonIcon
+</div>
+export default ThemeTogg1er;
+= useTheme();
